73846aa413d301c6d87a80de101db10dba93dfde
Rohit Mishra <rohitcr7mishra@gmail.com>
2025-07-03 02:43:18 +0530

    Update 217_ContainsDuplicate.java

--- Neetcode_150/Arrays&Hashing/217_ContainsDuplicate.java
+++ Neetcode_150/Arrays&Hashing/217_ContainsDuplicate.java
@@ -1,3 +1,6 @@
+// Time: O(n) - We iterate through the array once, and HashSet operations are O(1) on average
+// Space: O(n) - In the worst case (no duplicates), we store all n elements in the set
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer>set=new HashSet<>();
